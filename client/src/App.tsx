e80234a46be55d86a8bf6044756005099b543299
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-10-30 20:11:18 +0530

    frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./layout/Layout"
+import Home from "./pages/home/Home"
+
+const App = () => {
+  const router = createBrowserRouter([{
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }])
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
